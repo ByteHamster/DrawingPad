68e756e514751c80b0574283abfbc0577c8b2524
ByteHamster <info@bytehamster.com>
2019-01-28 23:24:23 +0100

    Replacing selected image, Closes #4

--- ankiAddon/__init__.py
+++ ankiAddon/__init__.py
@@ -6,6 +6,7 @@ from subprocess import call
 import re, os, shutil, time
 
 dp_mypath = os.path.dirname(os.path.abspath(__file__))
+dp_selected = "window.getSelection().getRangeAt(0).startContainer.parentNode.querySelector('img')"
 dp_fname = None;
 dp_drawing = None;
 dp_editor = None;
@@ -18,7 +19,7 @@ def drawingPad(ed):
     dp_editor = ed
     dp_fname = "drawingPad-"+checksum(str(time.time()))+".png"
     dp_drawing = os.path.join(dp_mypath, dp_fname)
-    dp_editor.web.evalWithCallback("window.getSelection().getRangeAt(0).cloneContents().querySelector('img').src", getSelectionCallback)
+    dp_editor.web.evalWithCallback(dp_selected + ".src", getSelectionCallback)
 
 def getSelectionCallback(imageSrc):
 
@@ -26,15 +27,19 @@ def getSelectionCallback(imageSrc):
         call(["java", "-jar", os.path.join(dp_mypath, "drawingPad.jar"), dp_drawing])
     else:
         imageSrc = imageSrc.split(os.sep).pop()
-        call(["java", "-jar", os.path.join(dp_mypath, "drawingPad.jar"), dp_drawing, "-i", imageSrc])
+        mdir = mw.col.media.dir()
+        call(["java", "-jar", os.path.join(dp_mypath, "drawingPad.jar"), dp_drawing, "-i", os.path.join(mdir, imageSrc)])
 
     if os.path.exists(dp_drawing):
         mdir = mw.col.media.dir()
         shutil.copyfile(dp_drawing, os.path.join(mdir, dp_fname))
-        dp_editor.web.eval("wrap(\"<img src='"+dp_fname+"' />\", \"\");")
+        if imageSrc == None:
+            dp_editor.web.eval("wrap(\"<img src='" + dp_fname + "' />\", \"\");")
+        else:
+            dp_editor.web.eval(dp_selected + ".src=\"" + dp_fname + "\";")
         os.remove(dp_drawing)
     else:
-        dp_editor.web.eval("wrap(\"Unable to start DrawingPad. Do you have JavaFx installed?\", \"\");")
+        dp_editor.web.eval("alert(\"Unable to start DrawingPad. Do you have JavaFx installed?\", \"\");")
 
 def addMyButton(buttons, editor):
     editor._links['drawingPad'] = drawingPad
