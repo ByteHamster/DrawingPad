8f15124f01ffbedc80cb64464e1137c91675c036
ByteHamster <info@bytehamster.com>
2019-08-21 14:24:03 +0200

    Enabled antialiasing

--- src/com/bytehamster/drawingpad/JCanvas.java
+++ src/com/bytehamster/drawingpad/JCanvas.java
@@ -4,13 +4,20 @@ import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class JCanvas extends JPanel {
     private final BufferedImage img;
+    private final Graphics2D graphics;
 
     public JCanvas(int width, int height) {
         img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        graphics = img.createGraphics();
+        graphics.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
         setLayout(null);
         setPreferredSize(new Dimension(width, height));
     }
@@ -29,7 +36,7 @@ public class JCanvas extends JPanel {
     }
 
     public Graphics2D getGraphics() {
-        return img.createGraphics();
+        return graphics;
     }
 
     public BufferedImage snapshot() {
